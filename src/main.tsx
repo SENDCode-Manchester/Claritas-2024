1ffc1e0b34808071a22d33786aef192be0d2bcdc
PixelBedrock <113065795+PixelBedrock@users.noreply.github.com>
2024-10-21 11:46:45 +0100

    chore: init

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([]);
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    {/* https://reactrouter.com/en/main/routers/create-browser-router */}
+    <RouterProvider router={router} />
+  </StrictMode>,
+);
